9b977d8c55b89067129e0e1fa689ff2cad8d6ead
callum stringer <callumstringer10@gmail.com>
2021-05-18 15:01:28 +0200

    :sparkles: sup

--- components/navLink.jsx
+++ components/navLink.jsx
@@ -5,8 +5,11 @@ export default function NavLink(properties) {
   const { href, text } = properties;
   return (
     <div className="sm:mr-4">
-      <Link href={href}>
-        <HighlightedText className="text-lg font-bold cursor-pointer">{text}</HighlightedText>
+      <Link href={href} passHref>
+        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+        <a>
+          <HighlightedText className="text-lg font-bold cursor-pointer">{text}</HighlightedText>
+        </a>
       </Link>
     </div>
   );
